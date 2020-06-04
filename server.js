628116a5b035447bfd2f787683cbc7e5dcee7472
AdamTronchin <atdraomnchin4@gmail.com>
2020-06-04 00:00:03 -0400

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const User = require('./models/User');
-mongoose.connect('mongodb://localhost/userData')
+mongoose.connect('mongodb://localhost/userData',{ useNewUrlParser: true, useUnifiedTopology: true })
 const port=8000;
 const app= express();
 
@@ -39,7 +39,35 @@ app.route('/users/:id')
 })
 // UPDATE
 .put((req,res)=>{
-  // User.findByIdAndUpdate()
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
+    },
+    {
+      new: true
+    },
+    (err, data)=>{
+      if(err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else{
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
 })
 // DELETE
 .delete((req,res)=>{
